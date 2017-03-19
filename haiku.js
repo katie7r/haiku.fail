79772bbb324f89b2721dc2ad86d8ed2c546cf064
Katie Russ <katie7r@users.noreply.github.com>
2017-03-19 18:36:35 -0400

    Update haiku.js
    List newest to oldest (add new haiku to the top)

--- haiku.js
+++ haiku.js
@@ -5,13 +5,13 @@ angular
 function haikuFactory() {
   // This is not the best way to do this... but it sure is easy!
   return [
-    ['Haiku break your heart -', 'Well, at least not break your mind', 'Or make you break wind!'],
-    ['Pollen falls like snow',   'Yellow thick on every car -',        'Tallahassee spring.'],
-    ['Being green is hard',      'As Kermit the Frog taught me -',     'But it is worth it.'],
-    ['Though I love haiku',      'Those I write are trivial -',        'I continue, still.'],
-    ['Every now and then,',      'I will try a real haiku -',          'Still just fit for fun.'],
+    ['Piña coladas',             'And getting caught in the rain:',    'Both things I enjoy.'],
     ['AngularJS',                'Seems excessive for this page,',     'But - you know - who cares?'],
-    ['Piña coladas',             'And getting caught in the rain:',    'Both things I enjoy.']
+    ['Every now and then,',      'I will try a real haiku -',          'Still just fit for fun.'],
+    ['Though I love haiku',      'Those I write are trivial -',        'I continue, still.'],
+    ['Being green is hard',      'As Kermit the Frog taught me -',     'But it is worth it.'],
+    ['Pollen falls like snow',   'Yellow thick on every car -',        'Tallahassee spring.'],
+    ['Haiku break your heart -', 'Well, at least not break your mind', 'Or make you break wind!'],
     // ['', '', ''],
   ];
 }
